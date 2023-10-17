165c492625176fa7ae85323a147c3bec13a2455f
Jeff Morhous <morhous.jeff@gmail.com>
2023-10-16 21:17:28 -0400

    Setup a groceryItem model and an in-memory data store

---
+++ models/groceryItem.go
@@ -0,0 +1,10 @@
+package models
+
+// GroceryItem represents an item in our grocery inventory.
+type GroceryItem struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Count    int    `json:"count"`
+	Category string `json:"category"`
+}
+
